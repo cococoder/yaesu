f9d229d77bafba7d36546fdb18135f1219e7395b
Coco Coder <shout@cococoder.com>
2021-02-27 18:10:44 +0200

    empty deploy

---
+++ lib/yaesu.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "yaesu/version"
+
+module Yaesu
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/yaesu/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Yaesu
+	VERSION = "0.0.0.alpha"
+end
