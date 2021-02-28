321b7f587a0b5e8f6a77b73c5cab005f0d6af636
Coco Coder <shout@cococoder.com>
2021-02-28 13:22:30 +0200

    Add configuration

--- lib/yaesu.rb
+++ lib/yaesu.rb
@@ -13,20 +13,40 @@ require 'fileutils'
 require_relative "yaesu/version"
 
 module Yaesu
+
+	def self.configuration
+		@configuration ||= OpenStruct.new(
+		  {
+			channel: "yaesu.local",
+			name: "base",
+			data_dir: "./.data"
+		  }
+		)
+	end
+
+	def self.configure
+		yield(configuration)
+	end
+
 	class Error < StandardError; end
 	def self.transmit on:, event:, data:
 		Ost[on] << {uid: UUID.new.generate,event:event, data: data}.to_msgpack
 	end
+	
+	def self.listen!
+		self.listen name:Yaesu.configuration.name, on: Yaesu.configuration.channel
+	end
+
 	def self.listen name:, on:
 		
-		FileUtils.mkdir_p(".data/#{name}") unless Dir.exists? ".data"
+		FileUtils.mkdir_p("#{Yaesu.configuration.data_dir}/#{name}") unless Dir.exists? ".data"
 		
-		puts "listening on #{on}...."
+		puts "#{name} is listening on #{on} -> Ok!"
 
 		begin
 			loop do
 				Ost[on].items.each do |data|		
-					SDBM.open "./.data/#{name}" do |db|
+					SDBM.open "#{Yaesu.configuration.data_dir}/#{name}" do |db|
 						o = MessagePack.unpack(data)
 						message =OpenStruct.new(o)
 						next if db.has_key? message.uid
@@ -41,7 +61,7 @@ module Yaesu
 				sleep 0.200
 			end
 		rescue Interrupt => e
-			puts "listening on #{on}....finished!"
+			puts "#{name} stopped listening on #{on} -> Ok!"
 		end
 	end
 end
--- lib/yaesu/version.rb
+++ lib/yaesu/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module Yaesu
-	VERSION = "1.0.3.beta"
+	VERSION = "1.1.0.beta"
 end
